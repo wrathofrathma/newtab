25729f506e8fe969da55509c3c7fd8d5cdef176c
Rathma <rathma@kyso.dev>
2022-04-02 17:48:19 -0400

    Using a layout abstraction now and started work on the layout

---
+++ src/layouts/Default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex justify-center items-center">
+    <slot name="image"></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,8 +1,23 @@
 <template>
-  <img alt="Vue logo" src="@/assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <default class="h-screen">
+    <template #image>
+      <img alt="Vue logo" src="@/assets/cat.gif" class="w-1/3" />
+    </template>
+
+    <div class="flex flex-col p-24 gap-16">
+      <search></search>
+      <div class="flex flex-row gap-20">
+        <link-tree category="work"></link-tree>
+        <link-tree category="dev"></link-tree>
+        <link-tree category="reddit"></link-tree>
+        <link-tree category="play"></link-tree>
+      </div>
+    </div>
+  </default>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
+import Default from "@/layouts/Default.vue";
+import LinkTree from "@/components/LinkTree.vue";
+import Search from "@/components/Search.vue";
 </script>
