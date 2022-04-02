bfa56c4c0c29f35561cc242662ff61a4bb2dff00
Rathma <rathma@kyso.dev>
2022-04-02 17:47:45 -0400

    Starting to build components for links & the search

---
+++ src/components/LinkTree.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="font-bold text-xl" v-if="category_link">
+      <a :href="category_link">{{ category }}</a>
+    </div>
+    <div class="font-bold text-xl" v-if="!category_link">
+      {{ category }}
+    </div>
+    <div>
+      <div
+        v-for="(link, index) in links"
+        class="text-gruvbox-dark-blue-1 text-base font-medium"
+        style=""
+      >
+        <a :href="link">{{ index }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  category: string;
+}>();
+
+import { useStore } from "../store";
+
+const store = useStore();
+
+const links = store.links(props.category);
+
+const category_link = store.categoryLink(props.category);
+</script>
---
+++ src/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="font-bold text-4xl">> cd ~/<span class="blinking">_</span></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.blinking {
+  animation: blink 1.5s ease-in-out infinite;
+  opacity: 1;
+}
+
+@keyframes blink {
+  0% {
+    opacity: 1;
+  }
+
+  50% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+</style>
